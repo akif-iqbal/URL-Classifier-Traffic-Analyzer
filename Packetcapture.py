73d830a367d9ba3d60fb024c636f00147dfe3080
vinaymvpes <vinaymvpes@gmail.com>
2022-10-30 12:25:05 +0530

    added classes for svm

---
+++ Packetcapture.py
@@ -0,0 +1,52 @@
+import pyshark
+#cap=pyshark.FileCapture(r'C:\Users\uveer\OneDrive\Documents\Project_capstone\p.cap.txt')
+capture=pyshark.LiveCapture()
+capture.sniff(timeout=10)
+arrayofpackets=[]
+for packet in capture.sniff_continuously(packet_count=5):
+    print(packet)
+    arofpackets=[]
+    #print(packet.ip.src)
+    #print
+    try:
+        if packet.ip:
+            arofpackets.append(packet.ip.src)
+            arofpackets.append(packet.length)
+        if packet.tcp:
+            arofpackets.append(packet.tcp.port)
+            '''arofpackets.append(packet.tcp.timestamps)'''
+        print(arofpackets)
+        arrayofpackets.append(arofpackets)
+    except:
+        print("attribute isnt available in packet")
+
+
+
+'''cap'''
+print(arrayofpackets)
+dir(packet.my_layer)
+
+def isddos(pkt):
+  if(pkt.tcp):
+        '''TCP SYN FLOOD'''
+        serverport={}
+        src=pkt[2]
+        port=pkt[3]
+        '''for i in range(len(pkt)):'''
+        if src in serverport:
+            serverport[src]=1+serverport.get(src,0)
+
+  if(pkt.udp):
+      '''UDP FLOOD '''
+      setport=set()
+      setsrc=set()
+      src=pkt[2]
+      port=pkt[3]
+      setport.add(port)
+      setsrc.add(src)
+      if len(setport)==len(setsrc):
+          print("Traffic is from normal source")
+      else:
+          print("UDP Flood")
+
+
