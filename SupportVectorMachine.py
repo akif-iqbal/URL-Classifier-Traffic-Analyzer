266563d0de5e0afd5223bb8b2bc7783422fa46df
akif-iqbal <akif.iqbal28@gmail.com>
2022-10-31 18:03:47 +0530

    added svm model

---
+++ SupportVectorMachine.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
+import pickle
+
+
+class SVM(object):
+	def process(self):
+		dataset = pd.read_csv('datasets/ids.csv')
+		dataset.drop("Timestamp",axis=1,inplace=True)
+		dataset["Flow Pkts/s"]=pd.to_numeric(dataset["Flow Pkts/s"],errors="coerce")
+		dataset.dropna(inplace=True)
+		dataset.replace([np.inf, -np.inf, np.nan], -1, inplace=True)
+		X = dataset.iloc[:, :-1].values
+		y = dataset.iloc[:, -1].values
+		self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+		self.X_train = np.nan_to_num(self.X_train)
+		self.X_test = np.nan_to_num(self.X_test)
+		sc=StandardScaler()
+		self.X_train = sc.fit_transform(self.X_train)
+		self.X_test = sc.transform(self.X_test)
+		return self.X_train,self.X_test,self.y_train,self.y_test
+
+	def training(self):
+		self.Xtrain,self.X_test,self.y_train,self.y_test=self.process()
+		self.classifier = SVC(kernel = 'linear', random_state = 0)
+		self.classifier.fit(self.X_train,self.y_train)
+		pickle.dump(self.classifier, open('build/svm_model.pkl', 'wb'))
+		return self.classifier
+
+	def testing(self):
+		svm_model = pickle.load(open('build/svm_model.pkl','rb'))
+		y_pred = svm_model.predict([[11,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,2000000,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,40,200,1000000,1000000,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,2,0,26883,0,0,80,0,0,0,0,0,0,0,0]])
+		return y_pred
+  
+if __name__=="__main__":
+	s=SVM()
+	# data=s.process()
+	# train=s.training()
+	test=s.testing()
+	print("Output: ", test)
