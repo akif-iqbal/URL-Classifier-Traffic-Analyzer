3ab932729b8a92cc5f76fc83efe7b688ac04e058
Vishal1904-ui <vishalupadyaya6786@gmail.com>
2022-12-01 23:20:50 +0530

    vecto file

--- log_pred.py
+++ log_pred.py
@@ -1,7 +1,13 @@
 from logistic import LogisReg
+import re
 
 ob=LogisReg()
-data=ob.process()
-web=["google.com/search=jcharistech"]
+web=["amazon.in"]
+
+for i in range(len(web)):
+    if "https://www." or "https://"  in web[i]:
+        url = re.compile(r"https?://(www\.)?")
+        web[i] = url.sub('', web[i]).strip().strip('/')
+print(web)
 pred=ob.testing(web)
 print(pred)
